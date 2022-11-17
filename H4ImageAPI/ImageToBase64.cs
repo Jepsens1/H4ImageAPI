8f031f9918981f00f10d412c3220a157720e8e46
Jepsens1 <jepsens123@outlook.dk>
2022-11-17 12:39:17 +0100

    Add project files.

---
+++ H4ImageAPI/ImageToBase64.cs
@@ -0,0 +1,11 @@
+﻿namespace H4ImageAPI
+{
+    public class ImageToBase64
+    {
+        public string ToBase64()
+        {
+            byte[] imageArray = File.ReadAllBytes("images/hogg.jpg");
+            return Convert.ToBase64String(imageArray);
+        }
+    }
+}
